aa11242ff44a4f34cf137e765f949b88bce0859a
laurens <laurens.krom@gmail.com>
2022-09-05 01:14:10 +0200

    added buttons and functionality to play round

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-  let playerSelection = "";
-  const compChoice = document.querySelector('.compChoice');
+  let cpuScore = 0;
+  let playerScore = 0;
   function getComputerChoice() {
     let i = Math.floor(Math.random()*3);
     switch (i) {
@@ -18,6 +18,7 @@
         case "rock":
           return `it's a tie, you both picked ${playerSelection}`
         case "paper":
+          cpuScore++;
           return `you lose, ${computerSelection} beats ${playerSelection}`
         case "scissors":
           return `you win! ${playerSelection} beats ${computerSelection}`
@@ -28,6 +29,7 @@
         case "paper":
           return `it's a tie, you both picked ${playerSelection}`
         case "scissors":
+          cpuScore++
           return `you lose, ${computerSelection} beats ${playerSelection}`
         case "rock":
           return `you win! ${playerSelection} beats ${computerSelection}`
@@ -38,23 +40,22 @@
         case "scissors":
           return `it's a tie, you both picked ${playerSelection}`
         case "rock":
+          cpuScore++
+          if (cpuScore == 5) {alert("you lost, resetting game"); cpuScore = 0}
           return `you lose, ${computerSelection} beats ${playerSelection}`
         case "paper":
           return `you win! ${playerSelection} beats ${computerSelection}`
       }
     }
   }
-  function getPlayerSelection() {
-    playerSelection = prompt("rock/paper/scissors?").toLowerCase()
-    while (playerSelection.toLowerCase() != "rock" && playerSelection.toLowerCase() != "paper" && playerSelection.toLowerCase() != "scissors") {
-      getPlayerSelection();
-    }
-      return playerSelection
-  }
 
-  function game() {
-    for (let i = 0; i < 5; i++) {
-      console.log(playRound(getPlayerSelection(), getComputerChoice()))
-    }
-  }
-  game();
+  //PLAYER SELECTION
+  const rockButton = document.getElementById("rock");
+  const paperButton = document.getElementById("paper")
+  const scissorsButton = document.getElementById("scissors")
+  rockButton.addEventListener("click", () => outputDiv.innerText = `${playRound("rock", getComputerChoice())} and the cpu score is now ${cpuScore}`);
+  paperButton.addEventListener("click", () => outputDiv.innerText = `${playRound("paper", getComputerChoice())} and the cpu score is now ${cpuScore}`);
+  scissorsButton.addEventListener("click", () => outputDiv.innerText = `${playRound("scissors", getComputerChoice())} and the cpu score is now ${cpuScore}`);
+  //output in div
+  const outputDiv = document.getElementById("output-div")
+
